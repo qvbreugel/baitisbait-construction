6ecb040cac5279941794f30b6c43c18d563e183e
Quinten van Breugel <qvbreugel@gmail.com>
2023-03-27 16:46:00 +0200

    Init commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Hind } from "next/font/google";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
+const hind = Hind({
+  variable: "--primary-font",
+  subsets: ["latin"],
+  weight: "300",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={hind.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
